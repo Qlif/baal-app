c8465884b8c23a787b82c2c4ed7e456befa98ff1
Qlif <baal72alex@gmail.com>
2020-04-07 02:13:53 +0300

    Add reselect and filter to JSON request

--- src/engine/core/crypto/saga/workers.js
+++ src/engine/core/crypto/saga/workers.js
@@ -6,8 +6,14 @@ import {setCryptoData} from "../../crypto/actions";
 
 export function* callCryptoDataAsyncWorker() {
     try {
-        const response = yield Axios.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=11")
-        const action = setCryptoData(response);
+        const response = yield Axios.get("https://api.kuna.io/v3/exchange-rates");
+        function filterCyrentCryppto(value) {
+            const cryptoValute = ["usd", "uah", "rub", "btc", "eth", "xrp"];
+            if(cryptoValute.includes(value.currency))
+                return value
+        }
+        const data = (response.data).filter(filterCyrentCryppto);
+        const action = setCryptoData(data);
         yield put(action)
     }catch (e) {
         console.log(e);
