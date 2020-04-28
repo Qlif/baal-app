c2d850cc5bdf3f7b7082ad46c39b4e46b8cb2675
Qlif <baal72alex@gmail.com>
2020-04-28 20:51:12 +0300

    Update method for request

--- src/engine/core/crypto/saga/workers.js
+++ src/engine/core/crypto/saga/workers.js
@@ -1,25 +1,28 @@
 // Modules
 import Axios from "axios";
-import {put} from "redux-saga/effects";
+import {put, delay} from "redux-saga/effects";
 // Action
 import {setCryptoData} from "../../crypto/actions";
 
 export function* callCryptoDataAsyncWorker() {
   const CURRENCY_FOR_CHECK = ['btc', 'eth', 'xrp'];
 
-  try {
-    const response = yield Axios.get("https://api.kuna.io/v3/exchange-rates");
-    const data = response.data.reduce((acc, item) => {
-      const {currency} = item;
-      if (CURRENCY_FOR_CHECK.includes(currency)) {
-        acc[currency] = item;
-      }
-      return acc;
-    }, {})
-    const action = setCryptoData(data);
-    yield put(action);
-  } catch (e) {
-    console.log(e);
+  while (true) { // Send request for update information
+    try {
+      const response = yield Axios.get("https://api.kuna.io/v3/exchange-rates");
+      const data = response.data.reduce((acc, item) => {
+        const {currency} = item;
+        if (CURRENCY_FOR_CHECK.includes(currency)) {
+          acc[currency] = item;
+        }
+        return acc;
+      }, {})
+      const action = setCryptoData(data);
+      yield put(action);
+      yield delay(300000);// 5 min wait
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
